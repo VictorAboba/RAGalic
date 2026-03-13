d623b95d3209323670fafd0e975eb502abd399c4
VictorAboba <153458099+VictorAboba@users.noreply.github.com>
2026-03-13 18:38:20 +0300

    ignore test files

--- tests.py
+++
@@ -1,7 +0,0 @@
-import re
-
-transfer_pattern = re.compile(r"(\S)-\s+(\S)|(\S)\s+-(\S)")
-
-text = "smt- h, dasdasdasd, smt - h, sdsaadasd smt-h, smt -h"
-
-print(re.sub(transfer_pattern, r"\1\2\3\4", text))
